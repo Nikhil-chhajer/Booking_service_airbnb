92463c0719576b5a7e6254915e0e282764c17649
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2025-06-16 19:43:27 +0530

    added lock with the idempotency key to handle multiple request from same user

--- src/repositories/booking.ts
+++ src/repositories/booking.ts
@@ -1,6 +1,7 @@
-import {  Prisma} from "@prisma/client";
+import {  IdempotencyKey, Prisma} from "@prisma/client";
 import  prismaClient  from "../prisma/client";
 import { InternalServerError } from "../utils/app.error";
+import { validate as isvalidUUID } from "uuid";
 
 export async function createBooking(bookingInput:Prisma.BookingCreateInput) {
   try {
@@ -16,7 +17,7 @@ export async function createBooking(bookingInput:Prisma.BookingCreateInput) {
 export async function createIdempotencyKey(idempotencyKey: string,bookingId:number) {
   try {
     const key = await prismaClient.idempotencyKey.create({
-      data: { key: idempotencyKey, 
+      data: { idemKey: idempotencyKey, 
         booking:{
           connect:{
             id: bookingId
@@ -30,12 +31,16 @@ export async function createIdempotencyKey(idempotencyKey: string,bookingId:numb
     throw new InternalServerError("Failed to create idempotency key");
   }
 }
-export async function getIdempotencyKey(idempotencyKey: string) {
+export async function getIdempotencyKeywithlock(tx:Prisma.TransactionClient,idempotencyKey: string) {
   try {
-    const key = await prismaClient.idempotencyKey.findUnique({
-      where: { key: idempotencyKey },
-    });
-    return key;
+    if(!isvalidUUID(idempotencyKey)){
+      throw new InternalServerError("Invalid idempotency key format");
+    }
+    const key:Array<IdempotencyKey> = await tx.$queryRaw(Prisma.raw(`SELECT * FROM IdempotencyKey WHERE idemKey = "${idempotencyKey}" FOR UPDATE;`))
+    if(!key || key.length === 0) {
+      throw new InternalServerError("Idempotency key not found");
+    }
+    return key[0];
   } catch (error) {
     console.error("Error retrieving idempotency key:", error);
     throw new InternalServerError("Failed to retrieve idempotency key");
@@ -52,9 +57,9 @@ export async function getBookingById(bookingId: number) {
     throw new InternalServerError("Failed to retrieve booking");
   }
 }
-export async function confirmBooking(bookingId: number) {
+export async function confirmBooking(tx:Prisma.TransactionClient,bookingId: number) {
   try {
-    const booking = await prismaClient.booking.update({
+    const booking = await tx.booking.update({
       where: { id: bookingId },
       data: { status: "CONFIRMED" },
     });
@@ -76,10 +81,10 @@ export async function cancelBooking(bookingId: number) {
     throw new InternalServerError("Failed to cancel booking");
   }
 }
-export async function finalizeIdempotencyKey(idempotencyKey: string) {
+export async function finalizeIdempotencyKey(tx:Prisma.TransactionClient,idempotencyKey: string) {
   try {
-    const key = await prismaClient.idempotencyKey.update({
-      where: { key: idempotencyKey },
+    const key = await tx.idempotencyKey.update({
+      where: { idemKey: idempotencyKey },
       data: { finalized: true },
     });
     return key;
--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -1,11 +1,12 @@
 
-import { confirmBooking, createBooking } from "../repositories/booking";
+import { confirmBooking, createBooking, getIdempotencyKeywithlock } from "../repositories/booking";
 import { createIdempotencyKey } from "../repositories/booking"; 
 import { generateIdempotencyKey } from "../utils/generateidempotencyKey";
-import { getIdempotencyKey } from "../repositories/booking";
 import { InternalServerError } from "../utils/app.error";
+import prismaClient from "../prisma/client";
 import { finalizeIdempotencyKey } from "../repositories/booking";
 import { createBookingDto } from "../dto/booking.dto";
+
 
 export async function createBookingService(BookingDto:createBookingDto) {
     try {
@@ -19,8 +20,9 @@ export async function createBookingService(BookingDto:createBookingDto) {
     
 }
 export async function confirmBookingService(idempotencyKey: string) {
-    try {
-        const idempotencyKeyData = await getIdempotencyKey(idempotencyKey);
+    return await prismaClient.$transaction(async (tx) => {
+        // Check if the idempotency key exists and is not finalized         
+        const idempotencyKeyData= await getIdempotencyKeywithlock(tx,idempotencyKey);
         if(!idempotencyKeyData) {
             throw new InternalServerError("Idempotency key not found");   
         }
@@ -28,17 +30,13 @@ export async function confirmBookingService(idempotencyKey: string) {
             console.log("Idempotency key data",idempotencyKeyData);
             throw new InternalServerError ("Booking already finalized" );
         }
-        //paymen here
-        const booking = await confirmBooking(idempotencyKeyData.bookingId);
-        await finalizeIdempotencyKey(idempotencyKey);
+        //payment here
+        const booking = await confirmBooking(tx,idempotencyKeyData.bookingId);
+        await finalizeIdempotencyKey(tx,idempotencyKey);
         return booking;
+    })
         
-    } catch (error) {
-        if(error instanceof InternalServerError) {
-            throw error; // Re-throw known errors   
-        }
-        throw new InternalServerError("Error finalizing booking");
-    }
+   
 }
 // export async function finalizeBookingService(bookingId: string) {
 //     try {
